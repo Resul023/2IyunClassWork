4016cee6db5fb0e5ba797106986de77ecbaec533
Resul023 <73m6u7h@code.edu.az>
2022-06-02 16:49:18 +0400

    ClassWork

---
+++ JuanPr/JuanPr/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuanPr.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+        public double oldPrice { get; set; }
+        public string mainIcon { get; set; }
+        public string Icon1 { get; set; }
+        public string Icon2 { get; set; }
+
+    }
+}
