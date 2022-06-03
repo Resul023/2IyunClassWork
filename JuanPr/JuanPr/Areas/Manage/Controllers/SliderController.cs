8f386324c9adcd0452b3bf9f3f08f3040bd95f48
Resul023 <73m6u7h@code.edu.az>
2022-06-03 16:17:56 +0400

    ClassWork

---
+++ JuanPr/JuanPr/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,31 @@
+﻿using JuanPr.Areas.ViewModel;
+using JuanPr.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuanPr.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+
+            ViewModelSlider sliderVW = new ViewModelSlider
+            {
+                Sliders = _context.Sliders.ToList(),
+            
+            };
+            return View(sliderVW);
+        }
+    }
+}
